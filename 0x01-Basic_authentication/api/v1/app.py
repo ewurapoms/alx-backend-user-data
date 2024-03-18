f290077d93162763ffb0e92e6df88dda75175e68
ewurapoms <mzpomaa35@gmail.com>
2024-03-18 14:38:23 -0700

    fixing pycodestyle errors

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,12 +12,13 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-AUTH_TYPE = os.getenv("AUTH_TYPE")
-if AUTH_TYPE == "auth":
+auth_type = getenv("AUTH_TYPE")
+if auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+elif auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
@@ -25,17 +26,17 @@ elif AUTH_TYPE == "basic_auth":
 @app.before_request
 def bef_req():
     """
-    Filter each request before it's handled by the proper route
+    Filtering of each request
     """
     if auth is None:
         pass
     else:
-        excluded = [
+        excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/'
         ]
-        if auth.require_auth(request.path, excluded):
+        if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
@@ -51,14 +52,16 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Request unauthorized handler
+    """
+    Error handler: Unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Request unauthorized handler
+    """
+    Error handler: Forbidden
     """
     return jsonify({"error": "Forbidden"}), 403
 
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,7 +10,8 @@ from models.user import User
 
 
 class BasicAuth(Auth):
-    """ Implement Basic Authorization protocol methods
+    """
+    Implement Basic Authorization protocol methods
     """
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
