b3da00648b52b260ab8dc4195a32c0c30ab17de0
ewurapoms <mzpomaa35@gmail.com>
2024-03-20 11:46:09 -0700

    modifying session_db_auth file

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -5,11 +5,11 @@ from models.base import Base
 
 
 class UserSession(Base):
-    """User session class.
+    """User session class
     """
 
     def __init__(self, *args: list, **kwargs: dict):
-        """Initializes a User session instance.
+        """Initializes a User session instance
         """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
