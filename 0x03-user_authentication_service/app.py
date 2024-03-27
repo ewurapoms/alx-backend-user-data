b7f60b06caa9d0f1c21f92ad76263817a102cedd
ewurapoms <mzpomaa35@gmail.com>
2024-03-27 01:23:49 -0700

    fixing pycodestyle error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -40,6 +40,7 @@ def login() -> str:
     response.set_cookie("session_id", session_id)
     return response
 
+
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout():
     """logs user out of the session"""
